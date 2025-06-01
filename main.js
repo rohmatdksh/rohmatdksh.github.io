9991360cb070c8da90f1f18bf620a4b039b29fef
Rohmatullah <101172529+rohmatdksh@users.noreply.github.com>
2025-06-01 20:01:08 +0700

    Upload manual
    belajar upload manual

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Computer Science", "Frontend Developer", "Web Developer", "Data Analist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true
+});
